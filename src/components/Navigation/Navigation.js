0f68581f47792e17785c5a951c954e74b990a464
Mike <mikegfisher@mfisher-mbp.local>
2018-02-06 22:38:36 -0800

    transpose links

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -9,8 +9,8 @@ const Navigation = () =>
     <div class="nav-wrapper">
       <Link class="brand-logo" to={routes.LANDING}>Unknown Stag</Link>
       <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><Link to={routes.LOGIN}>Log in</Link></li>
         <li><Link to={routes.SESSIONS}>Sessions</Link></li>
+        <li><Link to={routes.LOGIN}>Log in</Link></li>
       </ul>
     </div>
   </nav>
