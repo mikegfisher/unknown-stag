e1fdfa92e16f677e40d71cb777a7a94d69d2705d
Jeremiah Renfro <jrenfro@riskalyze.com>
2018-02-07 22:35:12 -0800

    Added  npm package,  file,  script, updated  with instructions for switching between environments, removed api key and messaging sender id from code

--- src/fire.js
+++ src/fire.js
@@ -1,27 +1,17 @@
 import firebase from 'firebase'
+import dotenv from 'dotenv'
 
-const prod = {
-  apiKey: "AIzaSyCIs2dqas2W0cP01QGOBzktScO0jeArv8k",
-  authDomain: "unknown-stag.firebaseapp.com",
-  databaseURL: "https://unknown-stag.firebaseio.com",
-  projectId: "unknown-stag",
-  storageBucket: "unknown-stag.appspot.com",
-  messagingSenderId: "252990311284"
-};
-const fire = firebase.initializeApp(prod);
+dotenv.load();
 
-/* USE THIS FOR LOCAL DEV AND COMMENT OUT THE ABOVE
- 
-const dev = {
-  apiKey: YOUR_API_KEY,
-  authDomain: YOUR_AUTH_DOMAIN,
-  databaseURL: YOUR_DATABASE_URL,
-  projectId: YOUR_PROJECT_ID,
-  storageBucket: '',
-  messagingSenderId: YOUR_MESSAGING_SENDER_ID
-}; 
-const fire = firebase.initializeApp(dev);
-
-*/
+/** @namespace process.env.API_KEY */
+/** @namespace process.env.MESSAGING_SENDER_ID */
+const fire = firebase.initializeApp({
+    apiKey: process.env.API_KEY,
+    authDomain: "unknown-stag.firebaseapp.com",
+    databaseURL: "https://unknown-stag.firebaseio.com",
+    projectId: "unknown-stag",
+    storageBucket: "unknown-stag.appspot.com",
+    messagingSenderId: process.env.MESSAGING_SENDER_ID
+});
 
 export default fire;
