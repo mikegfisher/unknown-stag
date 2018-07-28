3b99936345c2251dface38f6dde32f45456c5ec9
mikegfisher <mike@mikegfisher.com>
2018-07-27 21:22:40 -0700

    add testing for UnestimatedIssue

--- src/components/UnestimatedIssue/UnestimatedIssue.test.js
+++ src/components/UnestimatedIssue/UnestimatedIssue.test.js
@@ -1,10 +1,15 @@
 import '../../polyfill.js'
 import React from 'react';
 import ReactDOM from 'react-dom';
+import fire from '../../fire';
 import UnestimatedIssue from './UnestimatedIssue';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<UnestimatedIssue key={"unestimated_index"} id={"issueid"} owner={"this.state.owner"} title={"issue.title"} estimated={false} />, div);
+  let component = ReactDOM.render(<UnestimatedIssue key={"unestimated_index"} id={"issueid"} owner={"this.state.owner"} title={"issue.title"} estimated={false} />, div);
+  let comp = ReactDOM.findDOMNode(component);
+  component.updateIssue();
+  component.removeIssue();
+  component.submitEstimate();
   ReactDOM.unmountComponentAtNode(div);
 });
